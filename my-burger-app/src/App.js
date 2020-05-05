fe4335f3faad17015ee30c218af162e904ed0058
aman-ttn <aman.saini@tothenew.com>
2020-05-05 19:14:23 +0530

    My Burger Builder Assignment

--- my-burger-app/src/App.js
+++ my-burger-app/src/App.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Layout>
           <BurgerBuilder />
         </Layout>
