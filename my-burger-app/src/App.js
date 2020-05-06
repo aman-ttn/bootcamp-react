6b04d8b25f8bcbedf154b244e70e76e6a812e415
aman-ttn <aman.saini@tothenew.com>
2020-05-06 23:22:01 +0530

    Implemented axios ,fetching and storing the data from firebase api's

---
+++ my-burger-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+import Layout from './hoc/Layout/Layout';
+
+class App extends Component {
+ 
+
+  
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder /> 
+        </Layout>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
