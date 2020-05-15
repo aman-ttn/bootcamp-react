22b3d431e7378c7b2ac6b4d14c99379ce35d0212
aman-ttn <aman.saini@tothenew.com>
2020-05-15 08:41:55 +0530

    Implemented Authentication

---
+++ my-burger-app/src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+
+
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from './hoc/Layout/Layout';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
+import { connect } from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(()=> {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(()=> {
+  return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(()=> {
+  return import('./containers/Auth/Auth');
+});
+
+class App extends Component {
+
+  componentDidMount(){
+    this.props.onTryAutoSignup();
+  }
+ 
+render() {
+
+    let routes = (
+    <Switch>
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/auth" component={asyncAuth} />
+        <Redirect to="/"/>
+    </Switch>
+    );
+    if(this.props.isAuthenticated){
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={asyncCheckout} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/orders" component={asyncOrders} />
+            <Route path="/auth" component={asyncAuth} />
+            <Route path="/" exact component={BurgerBuilder} />
+            <Redirect to="/"/>
+        </Switch>
+
+      );
+    }
+
+    return (
+      <div>
+        <Layout>
+          {routes}
+        </Layout>
+        
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return{
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return{
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
